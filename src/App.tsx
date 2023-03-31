ac23e70bc18c4acb7bacc9308b5ec29dcd7cbf7c
Farfama <farfama.hargaaya@gmail.com>
2023-04-01 02:21:23 +0900

    Update Pagination
    - Pagination added to search results using react-paginate

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
 import { useState } from "react";
-import { searchMovies, SearchResult } from "./api";
+import { searchMovies, SearchResult, SearchResultItem } from "./api";
 import SearchForm from "./components/SearchForm";
 import MovieCard from "./components/MovieCard";
 import Logo from "./components/Logo";
+import ReactPaginate from "react-paginate";
 
 function App() {
   const [searchResults, setSearchResults] = useState<SearchResult["Search"]>(
     []
   );
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearch = (query: string) => {
-    searchMovies(query).then((data: SearchResult) => {
+    setSearchQuery(query);
+    searchMovies(query, 1).then((data: SearchResult) => {
       setSearchResults(data.Search);
+      setTotalPages(Math.ceil(parseInt(data.totalResults) / 10));
+      setCurrentPage(1);
+    });
+  };
+
+  const handlePageChange = (selectedItem: { selected: number }) => {
+    const pageNumber = selectedItem.selected + 1;
+    searchMovies(searchQuery, pageNumber).then((data: SearchResult) => {
+      setSearchResults(data.Search);
+      setCurrentPage(pageNumber);
     });
   };
 
   return (
     <div className="flex justify-center w-full ">
       <div>
-        <div className=" top-0 w-screen	bg-slate-50	z-10 shadow-lg p-5">
+        <div className="top-0 w-screen bg-slate-50 z-10 shadow-lg p-5">
           <div className="flex flex-col items-center justify-center py-16 ">
             <h1 className="text-4xl font-bold">Movie Search</h1>
-            <Logo/>
+            <Logo />
             <SearchForm onSubmit={handleSearch} />
           </div>
         </div>
@@ -31,6 +46,19 @@ function App() {
               <MovieCard key={index} movie={movie} />
             ))}
         </ul>
+        {totalPages > 0 && (
+          <ReactPaginate
+            pageCount={Math.ceil(totalPages / 10)}
+            marginPagesDisplayed={1}
+            pageRangeDisplayed={1}
+            onPageChange={handlePageChange}
+            containerClassName="pagination"
+            activeClassName="active-class"
+            nextLabel=""
+            previousLabel=""
+            breakLabel=".."
+          />
+        )}
       </div>
     </div>
   );
--- src/api.ts
+++ src/api.ts
@@ -21,11 +21,13 @@ export type SearchResultItem = {
 const OMDB_API_URL = "https://www.omdbapi.com";
 const OMDB_API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 
-export const searchMovies = async (query: string): Promise<SearchResult> => {
-  const response = await fetch(
-    `${OMDB_API_URL}/?apikey=${OMDB_API_KEY}&s=${query}`
-  );
-  const data = await response.json();
-  console.log(data);
-  return data;
-};
+export const searchMovies = async (
+    query: string,
+    page: number
+  ): Promise<SearchResult> => {
+    const response = await fetch(
+      `${OMDB_API_URL}/?apikey=${OMDB_API_KEY}&s=${query}&page=${page}`
+    );
+    const data = await response.json();
+    return data;
+  };
