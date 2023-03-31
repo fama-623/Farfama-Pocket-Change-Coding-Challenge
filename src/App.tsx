1ac1dff61cae71ee6b71abc35d302a2d5df39561
Farfama <farfama.hargaaya@gmail.com>
2023-03-31 17:47:06 +0900

    Update Search
    - Simple implementation of movie search bar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import { useEffect, useState } from 'react';
-import { searchMovies, SearchResult } from './api';
+import { useEffect, useState } from "react";
+import { searchMovies, SearchResult } from "./api";
 
 function App() {
-  const [searchResults, setSearchResults] = useState<SearchResult['Search']>([]);
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState<SearchResult["Search"]>(
+    []
+  );
 
-  useEffect(() => {
-    searchMovies('Batman').then((data: SearchResult) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    searchMovies(query).then((data: SearchResult) => {
       setSearchResults(data.Search);
     });
-  }, []);
+  };
+
   return (
     <div className="App">
       <h1>Movie Search App</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="query">Search movies:</label>
+        <input
+          type="text"
+          id="query"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
       <div className="search-results">
-        {searchResults.map((result, index) => (
-          <div key={index}>
-            <h2>{result.Title}</h2>
-            <p>Year: {result.Year}</p>
-            <p>Type: {result.Type}</p>
-            <img src={result.Poster} alt={`Poster for ${result.Title}`} />
-          </div>
-        ))}
+        {searchResults &&
+          searchResults.map((result, index) => (
+            <div key={index}>
+              <h2>{result.Title}</h2>
+              <p>Year: {result.Year}</p>
+              <p>Type: {result.Type}</p>
+              <img src={result.Poster} alt={`Poster for ${result.Title}`} />
+            </div>
+          ))}
       </div>
     </div>
   );
