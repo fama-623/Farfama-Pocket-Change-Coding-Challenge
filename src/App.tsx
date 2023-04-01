5c211d390e1826e8482223a7f5b14713c5a324cd
Farfama <farfama.hargaaya@gmail.com>
2023-04-02 00:28:55 +0900

    Add useMovieSearch custom hook
    - Business logic seperated from view

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from "react";
-import { searchMovies, SearchResult, SearchResultItem } from "./api";
+import ReactPaginate from "react-paginate";
 import SearchForm from "./components/SearchForm";
 import MovieCard from "./components/MovieCard";
 import Logo from "./components/Logo";
-import ReactPaginate from "react-paginate";
+import { useMovieSearch } from "./hooks/useMovieSearch";
 
 function App() {
-  const [searchResults, setSearchResults] = useState<SearchResult["Search"]>(
-    []
-  );
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const [searchQuery, setSearchQuery] = useState("");
-
-  const handleSearch = (query: string) => {
-    setSearchQuery(query);
-    searchMovies(query, 1).then((data: SearchResult) => {
-      setSearchResults(data.Search);
-      setTotalPages(Math.ceil(parseInt(data.totalResults) / 10));
-      setCurrentPage(1);
-    });
-  };
-
-  const handlePageChange = (selectedItem: { selected: number }) => {
-    const pageNumber = selectedItem.selected + 1;
-    searchMovies(searchQuery, pageNumber).then((data: SearchResult) => {
-      setSearchResults(data.Search);
-      setCurrentPage(pageNumber);
-    });
-  };
+  const {
+    searchResults,
+    totalPages,
+    handleSearch,
+    handlePageChange
+  } = useMovieSearch();
 
   return (
     <div className="flex justify-center w-full ">
---
+++ src/hooks/useMovieSearch.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { searchMovies, SearchResult } from "../api";
+
+export function useMovieSearch() {
+  const [searchResults, setSearchResults] = useState<SearchResult["Search"]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+    searchMovies(query, 1).then((data: SearchResult) => {
+      setSearchResults(data.Search);
+      setTotalPages(Math.ceil(parseInt(data.totalResults) / 10));
+      setCurrentPage(1);
+    });
+  };
+
+  const handlePageChange = (selectedItem: { selected: number }) => {
+    const pageNumber = selectedItem.selected + 1;
+    searchMovies(searchQuery, pageNumber).then((data: SearchResult) => {
+      setSearchResults(data.Search);
+      setCurrentPage(pageNumber);
+    });
+  };
+
+  return {
+    searchResults,
+    currentPage,
+    totalPages,
+    searchQuery,
+    handleSearch,
+    handlePageChange
+  };
+}
